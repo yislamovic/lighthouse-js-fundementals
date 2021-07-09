29b337263cd4843ff898b66ab007c7329f6fef04
Yahya Islamovic <yahya.islamovic.work@live.com>
2021-07-08 20:31:35 -0400

    packinglist

---
+++ packing-list.js
@@ -0,0 +1,9 @@
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("Kitchen stuff to pack:");
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
+console.log(packingList[0]);
+console.log(packingList[1]);
+console.log(packingList[2]);
