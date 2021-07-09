88901891f8eb99dda07a262fa59c1753cd61ced8
Yahya Islamovic <yahya.islamovic.work@live.com>
2021-07-08 20:33:22 -0400

    with for loop

--- packing-list.js
+++ packing-list.js
@@ -1,8 +1,10 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("Kitchen stuff to pack:");
-for (let i = 0; i < packingList.length; i++) {
+let i = 0;
+while (i < packingList.length) {
   console.log(packingList[i]);
+  i++;
 }
 console.log(packingList[0]);
 console.log(packingList[1]);
