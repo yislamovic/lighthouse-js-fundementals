2b1e5d635a8ad9430ecb41920aa3f396a1cadfaa
Yahya Islamovic <yahya.islamovic.work@live.com>
2021-07-08 20:00:56 -0400

    add more code examples

--- conditions.js
+++ conditions.js
@@ -1,4 +1,8 @@
 const temperature = 30;
+const isCitizen = true;
+const age = 26;
+const raining = true;
+const cold = false;
 
 if (temperature < 0) {
   console.log("Make sure you pick out a scarf!");
@@ -6,6 +10,12 @@ if (temperature < 0) {
   console.log("Short sleeves won't cut it!");
 } else {
   console.log("Short sleeves are fine.");
+}
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+if (!raining) {
+  console.log("Leave your umbrella at home!");
 }
 
 console.log("Now you're ready to go outside!");
